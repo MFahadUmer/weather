f153fe3e7fdc15a6a6a02c3c166d71fb7171f8e9
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2020-09-29 10:05:35 +0500

    Configure Assets of Webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpg';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
 
   return element;
 }
