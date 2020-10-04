bc25e9ede6534fdf62c1af5ee70eb6cdfe8ef9f5
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2020-10-04 21:05:53 +0500

    Adds Giphy Api for the background of Weather Details

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import navbar from "./navbar";
 import weatherApi from "./weatherApi";
-// import gihpyApi from "./giphyApi";
+import gihpyApi from "./giphyApi";
 import weatherBG from './weatherImage.jpg'
 
 searchForm.onsubmit = (e) =>{
@@ -10,28 +10,28 @@ searchForm.onsubmit = (e) =>{
   const a = weatherApi.weatherJson(search);
   let record = a.then(data => {
     if (data.cod == 200){
-      console.log(data);
-      console.log(data.weather);
-      console.log(data.weather[0].main);
       const temp = document.getElementById("temperature");
-      const startPage = document.getElementById("startPage");
+      const temperatureDetailsBG = document.getElementById("temperatureDetailsBG");
       temp.style.display = 'block';
-      startPage.style.display = 'none';
+      const giphy = gihpyApi.giphyJson(`${data.weather[0].main} weather`);
+      let giphyRecord = giphy.then((data) => {
+        temp.style.backgroundImage = `url(${data.data.images.downsized.url})`;
+      });
       temp.innerHTML = `
       <h2 class="weatherHeading pt-2">Weather Details of ${data.name}</h2>
         <div class="weatherDetails">
             <h3 class="bg-white">Country</h3>
             <h3 class="bg-white">${data.sys.country}</h3>
             <h3 class="bg-white">Description</h3>
-            <h3 class="bg-white">${data.weather[0].main}</h3>
+            <h3 class="bg-white">${data.weather[0].description}</h3>
             <h3 class="bg-white">Temperature</h3>
-            <h3 class="bg-white">${data.main.temp}</h3>
+            <h3 class="bg-white">${Math.floor(data.main.temp - 273.15, 2)}<sup><b>o</b></sup>C</h3>
             <h3 class="bg-white">Feels Like</h3>
-            <h3 class="bg-white">${data.main.feels_like}</h3>
+            <h3 class="bg-white">${Math.floor(data.main.feels_like - 273.15, 2)}<sup><b>o</b></sup>C</h3>
             <h3 class="bg-white">Minimum Temperature</h3>
-            <h3 class="bg-white">${data.main.temp_min}</h3>
+            <h3 class="bg-white">${Math.floor(data.main.temp_min - 273.15, 2)}<sup><b>o</b></sup>C</h3>
             <h3 class="bg-white">Maximum Temperature</h3>
-            <h3 class="bg-white">${data.main.temp_max}</h3>
+            <h3 class="bg-white">${Math.floor(data.main.temp_max - 273.15,2 )}<sup><b>o</b></sup>C</h3>
             <h3 class="bg-white">Pressure</h3>
             <h3 class="bg-white">${data.main.pressure}</h3>
             <h3 class="bg-white">Sea Level</h3>
@@ -47,11 +47,3 @@ searchForm.onsubmit = (e) =>{
   })
 };
 document.getElementById('weatherImg').setAttribute('src', weatherBG);
-
-// const giphy = gihpyApi.giphyJson('');
-// let giphyRecord = giphy.then((data) => {
-//   // console.log(data.data.embed_url);
-//   document.getElementById('weatherImg').setAttribute('src', data.data.embed_url);
-// }).catch(() => {
-//   document.getElementById('weatherImg').setAttribute('src', 'https://giphy.com/embed/GwGXoeb0gm7sc');
-// })
